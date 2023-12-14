89e1c6c967116c97678956ee4f28c4e1b07a542c
m4linolsson <malin.olsson.java23@edu.edugrade.se>
2023-12-14 08:38:28 +0100

    Join mellan Match och Player. Funkar att ändra player i matchen.

--- src/main/java/com/teamalpha/teamalphapipergames/HelloApplication.java
+++ src/main/java/com/teamalpha/teamalphapipergames/HelloApplication.java
@@ -10,41 +10,41 @@ import javafx.stage.Stage;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        MatchGraphics matchGraphics = new MatchGraphics();
-        matchGraphics.start(new Stage());
+//        MatchGraphics matchGraphics = new MatchGraphics();
+//        matchGraphics.start(new Stage());
     }
 
     public static void main(String[] args) {
 
 
-        MatchController matchController = new MatchController();
-        PlayerController playerController = new PlayerController();
-
-
-
-        playerController.save(new Player("Valerii", "Vakhovskyi", "b1t", "Kosmonavtov", "NONE", "DNEPROPETROVSK", "Ukraine", "b1t@navi.com"));
-        playerController.save(new Player("Justinas", "Lekavicius", "jL", "52 Luknojų", "06295", "VILNIUS", "Lithuania", "jL@navi.com"));
-        playerController.save(new Player("Aleksi", "Virolainen", "Aleksib", "Kluuvikatu 5", "01380", "UUSIMAA", "Finland", "aleksib@navi.com"));
-        playerController.save(new Player("Mihai", "Ivan", "iM", "Nr.179B, Creaca", "NONE", "SALAJ", "Romania", "iM@navi.com"));
-        playerController.save(new Player("Ihor", "Zhdanov", "w0nderful", "Mineralnaya 18", "NONE", "IRPEN", "Ukraine", "w0nderful@navi.com"));
-        playerController.save(new Player("Lukas", "Rossander", "gla1ve", "Lumbyholmvej 39", "3390", "SJAELLAND", "Denmark", "gla1ve@ence.gg"));
-        playerController.save(new Player("Pawel", "Dycha", "dycha", "Chlodna 25", "40-311", "KATOWICE", "Poland", "dycha@ence.gg"));
-        playerController.save(new Player("Pavle", "Boskovic", "maden", "60, 19. Decembra", "81110", "PODGORICA", "Montenegro", "dycha@ence.gg"));
-        playerController.save(new Player("Alvaro", "Garcia", "SunPayus", "Puerta Nueva 62", "36380", "PONTEVEDRA", "Spain", "sunpayus@ence.gg"));
-        System.out.println("'''''''''''''''''''''''''");
-        matchController.addNewMatch(1,false,1,2,"sf");
-
-//        matchController.saveMatch(new Match(true, 1, 1, 2, false, "2023/12/24"));
-//        matchController.saveMatch(new Match(true, 1, 2, 4, true, "3455"));
-//        matchController.saveMatch(new Match(false, 1, 1, 4, true, "3455"));
-//        matchController.saveMatch(new Match(false, 1, 2, 4, true, "3455"));
-//        matchController.saveMatch(new Match(false, 1, 2, 4, true, "3455"));
-
-
-        System.out.println("'''''''''''''''''''''''''");
-
-
-     launch();
+//        MatchController matchController = new MatchController();
+//        PlayerController playerController = new PlayerController();
+//
+//
+//
+//        playerController.save(new Player("Valerii", "Vakhovskyi", "b1t", "Kosmonavtov", "NONE", "DNEPROPETROVSK", "Ukraine", "b1t@navi.com"));
+//        playerController.save(new Player("Justinas", "Lekavicius", "jL", "52 Luknojų", "06295", "VILNIUS", "Lithuania", "jL@navi.com"));
+//        playerController.save(new Player("Aleksi", "Virolainen", "Aleksib", "Kluuvikatu 5", "01380", "UUSIMAA", "Finland", "aleksib@navi.com"));
+//        playerController.save(new Player("Mihai", "Ivan", "iM", "Nr.179B, Creaca", "NONE", "SALAJ", "Romania", "iM@navi.com"));
+//        playerController.save(new Player("Ihor", "Zhdanov", "w0nderful", "Mineralnaya 18", "NONE", "IRPEN", "Ukraine", "w0nderful@navi.com"));
+//        playerController.save(new Player("Lukas", "Rossander", "gla1ve", "Lumbyholmvej 39", "3390", "SJAELLAND", "Denmark", "gla1ve@ence.gg"));
+//        playerController.save(new Player("Pawel", "Dycha", "dycha", "Chlodna 25", "40-311", "KATOWICE", "Poland", "dycha@ence.gg"));
+//        playerController.save(new Player("Pavle", "Boskovic", "maden", "60, 19. Decembra", "81110", "PODGORICA", "Montenegro", "dycha@ence.gg"));
+//        playerController.save(new Player("Alvaro", "Garcia", "SunPayus", "Puerta Nueva 62", "36380", "PONTEVEDRA", "Spain", "sunpayus@ence.gg"));
+//        System.out.println("'''''''''''''''''''''''''");
+//        matchController.addNewMatch(1,false,1,2,"sf");
+//
+////        matchController.saveMatch(new Match(true, 1, 1, 2, false, "2023/12/24"));
+////        matchController.saveMatch(new Match(true, 1, 2, 4, true, "3455"));
+////        matchController.saveMatch(new Match(false, 1, 1, 4, true, "3455"));
+////        matchController.saveMatch(new Match(false, 1, 2, 4, true, "3455"));
+////        matchController.saveMatch(new Match(false, 1, 2, 4, true, "3455"));
+//
+//
+//        System.out.println("'''''''''''''''''''''''''");
+//
+//
+//     launch();
     }
 }
 
